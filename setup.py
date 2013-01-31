a0981dd1c681612fb809f82cd5e59096c65eb432
Renata Carreira <renata.correia@corp.globo.com>
2013-01-31 15:17:00 -0200

    rtmp2img: setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from os.path import dirname, abspath, join
+from setuptools import setup
+
+with open(abspath(join(dirname(__file__), 'README.md'))) as fileobj:
+    README = fileobj.read().strip()
+
+setup(
+    name='rtmp2img',
+    description='Save images from rtmp live videos',
+    long_description=README,
+    author='Globo.com',
+    url='https://github.com/globocom/rtmp2img',
+    version='0.1.2',
+    zip_safe=False,
+    include_package_data=True,
+    entry_points={
+        'console_scripts':
+        ['rtmp2img = rtmp2img.cli:main'],
+        },
+    packages=[
+        'rtmp2img',
+        ],
+    install_requires=[
+        'requests==1.1.0',
+		'sh==1.07',
+		'wsgiref==0.1.2',
+        ],
+)
